4189b5a7f11f9bb44bee978ee29e0407f0215396
Trip <dwd2@pdx.edu>
2019-05-12 13:12:42 +0000

    Add new heroku message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render html: "Hello Trip!"
+      render html: "Hello Trip! Welcome to the World, Sir!"
   end 
   
 end
