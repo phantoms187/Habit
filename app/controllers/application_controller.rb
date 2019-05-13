92dc1bf3eb37938146e43c5edbdda62d6c5cf625
Trip <dwd2@pdx.edu>
2019-05-13 13:46:35 +0000

    Finished Static Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-<<<<<<< HEAD
       render html: "Good Morning Meg!"
-=======
-      render html: "Hello Trip! Welcome to the World!"
->>>>>>> static-pages
   end 
   
 end
