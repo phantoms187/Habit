f414925b38dfb0fa7642e2a9bcd08a383f9fce50
Trip <dwd2@pdx.edu>
2019-06-04 09:46:17 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,9 +26,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'b742d3f7b6b54c26a9db6cf5eb192e4a.vfs.cloud9.us-west-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
