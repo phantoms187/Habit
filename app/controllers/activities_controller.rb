541924f1b496d25bfcb88352b75e8adb72f6b972
Trip <dwd2@pdx.edu>
2019-07-02 13:19:41 +0000

    restore to working activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
        @activity = current_user.activities.build(activities_params)
@@ -12,11 +13,19 @@ class ActivitiesController < ApplicationController
   end
 
   def destroy
+    @activity.destroy
+    flash[:success] = "Activity deleted"
+    redirect_to request.referrer || root_url
   end
   
   private
 
     def activities_params
       params.require(:activity).permit(:climb, :duolingo, :teeth, :mia, :run, :keyboard, :pull, :floor, :bell)
+    end
+    
+    def correct_user
+      @activity = current_user.activities.find_by(id: params[:user_id])
+      redirect_to root_url if @activity.nil?
     end
 end
