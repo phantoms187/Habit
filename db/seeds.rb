b7ed784ca2f80c276c738b5f4860b4edecf30bff
Trip <dwd2@pdx.edu>
2019-07-02 13:02:40 +0000

    sqlite3 1.2.13 for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-User.create!(name:  "Trip",
+User.create!(name:  "Dave",
              email: "dave@email.edu",
              password:              "hockey",
              password_confirmation: "hockey",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
+
+User.create!(name:  "Meg",
+             email: "megdisario@gmail.com",
+             password:              "miaisthebest",
+             password_confirmation: "miaisthebest",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
+             
+User.create!(name:  "Batman",
+             email: "thebat@gmail.edu",
+             password:              "miaisthebest",
+             password_confirmation: "miaisthebest",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
+
+User.create!(name:  "Trip",
+             email: "warrend187@gmail.com",
+             password:              "megandmia",
+             password_confirmation: "megandmia",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
